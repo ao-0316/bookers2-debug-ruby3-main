75026bafc08bd0b9b1741d4d2cae4c20747d48aa
ao-0316 <aosho0316.yukiusagi@gmail.com>
2023-12-27 15:11:55 +0000

    2-2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,14 @@
 class BooksController < ApplicationController
 
   def show
+    @newbook = Book.new
     @book = Book.find(params[:id])
+    @user = @book.user
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def create
@@ -14,6 +17,7 @@ class BooksController < ApplicationController
     if @book.save
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
+      
       @books = Book.all
       render 'index'
     end
@@ -26,21 +30,22 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      redirect_to book_path(@book), notice: "You have updated book successfully."
+      redirect_to book_path(@book), flash[:notice] = "You have updated book successfully."
     else
+      
       render "edit"
     end
   end
 
-  def delete
+  def destroy
     @book = Book.find(params[:id])
-    @book.destoy
+    @book.destroy
     redirect_to books_path
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body,)
   end
 end
