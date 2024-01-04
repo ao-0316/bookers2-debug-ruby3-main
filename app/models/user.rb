d14564357181151c999bc803dbcb7520ca1b6d0c
ao-0316 <aosho0316.yukiusagi@gmail.com>
2024-01-04 08:34:23 +0000

    5-1

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
 
   has_many :books, dependent: :destroy
   
@@ -10,7 +11,15 @@ class User < ApplicationRecord
   
   has_many :favorites, dependent: :destroy
   
-  has_many :books, class_name: "モデル名", foreign_key: "○○_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  # has_many :xxx, class_name: "モデル名", foreign_key: "○○_id", dependent: :destroy
+  
+   has_many :followers, through: :reverse_of_relationships, source: :follower
+  # has_many :yyy, through: :xxx, source: :zzz
+  
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  
+  has_many :followings, through: :relationships, source: :followed
 
   has_one_attached :profile_image
 
@@ -19,7 +28,35 @@ class User < ApplicationRecord
 
 
 
-  def get_profile_image
-    (profile_image.attached?) ? profile_image : 'no_image.jpg'
+    def get_profile_image
+      (profile_image.attached?) ? profile_image : 'no_image.jpg'
+    end
+  
+      # フォローしたときの処理
+    def follow(user_id)
+      relationships.create(followed_id: user_id)
+    end
+    # フォローを外すときの処理
+    def unfollow(user_id)
+      relationships.find_by(followed_id: user_id).destroy
+    end
+    # フォローしているか判定
+    def following?(user)
+      followings.include?(user)
+    end
+    
+  def self.looks(search, word)
+      if search == "perfect_match"
+        @user = User.where("name LIKE?", "#{word}")
+      elsif search == "forward_match"
+        @user = User.where("name LIKE?","#{word}%")
+      elsif search == "backward_match"
+        @user = User.where("name LIKE?","%#{word}")
+      elsif search == "partial_match"
+        @user = User.where("name LIKE?","%#{word}%")
+      else
+        @user = User.all
+      end
   end
 end
+
