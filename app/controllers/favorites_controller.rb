2768c2684e221e87904ed218b88f9f9ad71f3c6a
ao-0316 <aosho0316.yukiusagi@gmail.com>
2024-01-07 02:45:32 +0000

    6-3

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,16 +2,19 @@ class FavoritesController < ApplicationController
 
   def create
     book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
-    favorite.save
+    @favorite = current_user.favorites.new(book_id: book.id)
+    @favorite.save
+    render 'replace_btn'
     # redirect_to request.referer
   end
 
   def destroy
     book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
-    favorite.destroy
+    @favorite = current_user.favorites.new(book_id: book.id)
+    @favorite.destroy
+    render 'replace_btn'
     # redirect_to request.referer
+
   end
 
 end
